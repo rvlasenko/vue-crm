5f837c97d48149b574f04f8b852cfbd5f0a21b21
Roman Vlasenko <rvlasenko.1997@gmail.com>
2020-07-22 12:23:56 +0400

    Layouts and routing was added

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,21 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    meta: {
+      layout: 'main'
+    },
+    name: 'Categories',
+    component: () => import('../views/Categories.vue')
+  },
 ]
 
 const router = new VueRouter({
